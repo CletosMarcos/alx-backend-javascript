df17577d66908d5241135651231fc1da104f4547
CletosMarcos <cletosmarcos@gmail.com>
2025-01-07 01:23:12 +0000

    add task

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdout.write('Welcome to ALX, what is your name?\n');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name !== null) {
-    process.stdout.write(`Your name is: ${name}`);
+    process.stdout.write('Your name is: ', name);
   }
 });
 process.on('exit', () => {
